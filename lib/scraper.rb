2373b10b3fd2d88f722d8cefb59ccd939f1ca7af
Kizito Njoku <nkizito57@gmail.com>
2019-11-24 05:02:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-# html = open("https://flatironschool.com/")
-# doc = Nokogiri::HTML(html)
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
 
-# # or 
+# or 
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-260IBN")
+# doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+puts doc.css(".headline-260IBN").text
