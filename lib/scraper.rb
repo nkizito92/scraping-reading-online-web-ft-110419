3b918886b019560f017e69a634cdedfe35961761
Kizito Njoku <nkizito57@gmail.com>
2019-11-24 05:04:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,4 +7,9 @@ doc = Nokogiri::HTML(html)
 # or 
 
 # doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css("#header-content > div > h1").text
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
