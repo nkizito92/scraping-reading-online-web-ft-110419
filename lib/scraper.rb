0d50d87e49e57ec623ce691a1078f1dac0c8e1fc
Kizito Njoku <nkizito57@gmail.com>
2019-11-24 05:56:11 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
+ 
+# html = open("https://flatironschool.com/")
+# doc = Nokogiri::HTML(html)
+ 
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+# doc.css("#header-content > div > h1").text 
+# puts doc
 
-html = open("https://flatironschool.com/")
+
+courses = doc.css("#header-content > div > h1")
+ 
+courses.collect do |course|
+  puts course.text.strip
+end
